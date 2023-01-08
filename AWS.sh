fc026849b181246aa48b5bd5d001b93e636b5537
Carbonhell <frafonia@gmail.com>
2023-01-08 12:51:25 +0100

    AWS.sh fixes, reworked tests to get a single resource since AB cannot follow linked resources

--- AWS.sh
+++ AWS.sh
@@ -39,18 +39,18 @@ fi
 
 
 webservers=("actix-web-bench" "rocket-bench" "axum-bench")
-test_folders=("Single" "Small" "Medium" "Large")
+test_folders=("Tiny" "Small" "Medium" "Large" "Dynamic")
 server_type=("Normal" "Compression" "SingleThreaded")
 concurrency_levels=("1" "25" "50" "75" "100" "150" "200")
 requests="20000"
 
 for webserver in "${webservers[@]}"; do
-  if [[ "${webserver}" != "actix-web-bench" ]]; then
-    commands=("cargo run --release" "cargo run --release --features compression -- -c" "cargo run --release -- -s")
-  elif [[ "${webserver}" != "rocket-bench" ]]; then
-    commands=("ROCKET_PORT=8080 cargo run --release" "ROCKET_PORT=8080 cargo run --release --features=compression" "ROCKET_PORT=8080 ROCKET_WORKERS=1 cargo run --release")
-  elif [[ "${webserver}" != "axum-bench" ]]; then
-    commands=("cargo run --release" "cargo run --release --features=compression" "cargo run --release --features=single_threaded")
+  if [[ "${webserver}" == "actix-web-bench" ]]; then
+    commands=("nohup cargo run --release >log.txt 2>&1 &" "nohup cargo run --release --features compression -- -c >log.txt 2>&1 &" "nohup cargo run --release -- -s >log.txt 2>&1 &")
+  elif [[ "${webserver}" == "rocket-bench" ]]; then
+    commands=("ROCKET_PORT=8080 nohup cargo run --release >log.txt 2>&1 &" "ROCKET_PORT=8080 nohup cargo run --release --features=compression >log.txt 2>&1 &" "ROCKET_PORT=8080 ROCKET_WORKERS=1 nohup cargo run --release")
+  elif [[ "${webserver}" == "axum-bench" ]]; then
+    commands=("nohup cargo run --release >log.txt 2>&1 &" "nohup cargo run --release --features=compression >log.txt 2>&1 &" "nohup cargo run --release --features=single_threaded >log.txt 2>&1 &")
   fi
 
   # For each web server type (Normal, Compression enabled, Forced single thread)
@@ -58,15 +58,6 @@ for webserver in "${webservers[@]}"; do
     # Run a test for each static workload configuration: Single, Small, Medium, Large
     for test_type_key in "${!test_folders[@]}"; do
       echo "Test type is ${test_folders[${test_type_key}]}."
-      # If not single, set the image to the correct size
-      if [ "${test_folders[${test_type_key}]}" != "Single" ]; then
-        echo "Setting static workload image size..."
-        sudo ssh -i "$sshkey" ec2-user@"$server_ip" <<EOF
-        cd ~/RustWebserversBenchmark-master
-        python3 set_workload_size.py ${test_type_key}
-EOF
-        echo "Set."
-      fi
 
       for concurrency_level in "${concurrency_levels[@]}"; do
         # Restart the server for each group of repetitions
@@ -78,22 +69,28 @@ EOF
           pkill cargo
           unset ROCKET_WORKERS
           cd ~/RustWebserversBenchmark-master/${webserver}
-          nohup ${commands[${command_key}]} >/dev/null 2>&1 &
+          nohup ${commands[${command_key}]} >log.txt 2>&1 &
 EOF
-        printf "Waiting for the server to be ready..."
+        printf "Waiting for the server to be ready... (This may take a few minutes due to the compilation step)"
         while ! sudo ssh -q -i "$sshkey" ec2-user@"$client_ip" "curl --output /dev/null --silent --head --fail http://${private_server_ip}:8080/single_page.html" ; do
             printf "."
-            sleep 5
+            sleep 20
         done
 
         echo "Web server ready."
 
         # Repeat each test thrice
         for i in "1" "2" "3"; do
-          if [[ "${test_folders[${test_type_key}]}" != "Single" ]]; then
-            target_page="index.html";
-          else
-            target_page="single_page.html";
+          if [[ "${test_folders[${test_type_key}]}" == "Tiny" ]]; then
+            target_page="tiny.html";
+          elif [[ "${test_folders[${test_type_key}]}" == "Small" ]]; then
+            target_page="prototype_kilogram_small.jpg";
+          elif [[ "${test_folders[${test_type_key}]}" == "Medium" ]]; then
+            target_page="prototype_kilogram_medium.jpg";
+          elif [[ "${test_folders[${test_type_key}]}" == "Large" ]]; then
+            target_page="prototype_kilogram_large.jpg";
+          elif [[ "${test_folders[${test_type_key}]}" == "Dynamic" ]]; then
+            target_page="dynamic?first=2&second=3"
           fi
           benchmark_cmd="ab -n ${requests} -c ${concurrency_level} -k http://${private_server_ip}:8080/${target_page} >${i}.txt 2>&1"
           echo "Benchmark command is: ${benchmark_cmd}"
